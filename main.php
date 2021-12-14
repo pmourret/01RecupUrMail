11758b5e1e3d622b7be9fc97d1f2876673d3f48f
AbyssionPM <pierre.mourret.cav@gmail.com>
2021-12-14 21:53:38 +0100

    MàJ_14/12/2021

--- main.php
+++ main.php
@@ -1,49 +1,47 @@
 <?php 
     include_once 'fonction.php';
-    //*********Déclaration variables**************/
-    
-    $valide = 0;
-    $nonValide = 0;
-    $connexion = connexion();
 
+    $connexion = connexion();
     if(!$connexion){
         echo 'Echec de la connexion';
     }
     else{
         echo 'Connexion établie : <br>';
-
         $infos = infoBMail($connexion);
         $entete = accesEntete($connexion, $infos);
-        $dateIn= stockDate($entete);
-        $dateOut = stockUdate($entete);
-
-        /******* Comparateur des dates ****************/
-        $substract = array();
-            for($i=0;$i<count($entete);$i++){ 
-                $substract[$i] = $dateOut[$i] - $dateIn[$i]; //remplissage du tableau substract
-                if($dateOut[$i] > $dateIn[$i]){
-                    $valide+= 1;
-                }
-                else{
-                    $nonValide+= 1;
-                }
-            }
-        echo "Il existe ".$valide." échanges valides pour "."$nonValide"." échanges non valide(s)";
     }
-
+    //*********Déclaration variables**************/
+    
+    $valide = 0;
+    $nonValide = 0;
+    $dateIn= stockDate($entete);
+    //$dateOut = stockUdate($entete);       OLD
+    $dateOut = getSentMail();            // NEW
+    
     /*************** Affichage *******************/
     
     //afficheTab($entete);
-    //afficheTab($dateIn);
-    //afficheTab($dateOut);
-    echo "<br>Nombre d'entrées dans le tableau entete : ".count($entete)."<br>";
+    afficheTab($dateIn);
+    echo "---------------<br/>\n";
+    afficheTab($dateOut);
+    echo "Nombre d'entrées dans le tableau entete : ".count($entete)."<br>";
     echo "Nombre d'entrées dans le tableau dateIn : ".count($dateIn)."<br>";
     echo "Nombre d'entrées dans le tableau dateOut : ".count($dateOut)."<br>";
 
- 
-    
+    /******* Comparateur des dates ****************/
+    $substract = array();
+    for($i=0;$i<count($entete);$i++){ 
+        $substract[$i] = $dateOut[$i] - $dateIn[$i]; //remplissage du tableau substract
+        if($dateOut[$i] > $dateIn[$i]){
+            $valide+= 1;
+        }
+        else{
+            $nonValide+= 1;
+        }
+    }
     
     echo "Il existe ".$valide." échanges valides pour ".$nonValide." échanges non valides";
+
 
     
 
