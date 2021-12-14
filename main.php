dff140b6473febef28a388e29993f590264f590a
AbyssionPM <pierre.mourret.cav@gmail.com>
2021-12-14 19:32:30 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,44 +1,48 @@
 <?php 
     include_once 'fonction.php';
-
+    //*********Déclaration variables**************/
+    
+    $valide = 0;
+    $nonValide = 0;
     $connexion = connexion();
+
     if(!$connexion){
         echo 'Echec de la connexion';
     }
     else{
         echo 'Connexion établie : <br>';
+
         $infos = infoBMail($connexion);
         $entete = accesEntete($connexion, $infos);
+        $dateIn= stockDate($entete);
+        $dateOut = stockUdate($entete);
+
+        /******* Comparateur des dates ****************/
+        $substract = array();
+            for($i=0;$i<count($entete);$i++){ 
+                $substract[$i] = $dateOut[$i] - $dateIn[$i]; //remplissage du tableau substract
+                if($dateOut[$i] > $dateIn[$i]){
+                    $valide+= 1;
+                }
+                else{
+                    $nonValide+= 1;
+                }
+            }
+        echo "Il existe ".$valide." échanges valides pour "."$nonValide"." échanges non valide(s)";
     }
-    //*********Déclaration variables**************/
-    
-    $valide = 0;
-    $nonValide = 0;
-    $dateIn= stockDate($entete);
-    $dateOut = stockUdate($entete);
 
     /*************** Affichage *******************/
     
     //afficheTab($entete);
     //afficheTab($dateIn);
     //afficheTab($dateOut);
-    echo "Nombre d'entrées dans le tableau entete : ".count($entete)."<br>";
+    echo "<br>Nombre d'entrées dans le tableau entete : ".count($entete)."<br>";
     echo "Nombre d'entrées dans le tableau dateIn : ".count($dateIn)."<br>";
     echo "Nombre d'entrées dans le tableau dateOut : ".count($dateOut)."<br>";
 
-    /******* Comparateur des dates ****************/
-    $substract = array();
-    for($i=0;$i<count($entete);$i++){ 
-        $substract[$i] = $dateOut[$i] - $dateIn[$i]; //remplissage du tableau substract
-        if($dateOut[$i] > $dateIn[$i]){
-            $valide+= 1;
-        }
-        else{
-            $nonValide+= 1;
-        }
-    }
+
     
-    echo "Il existe ".$valide." échanges valides pour "."$nonValide"." échanges non valides";
+    
 
 
     
