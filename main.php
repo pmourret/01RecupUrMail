42fe35b758925667b58cd209f32efca81b66e82c
AbyssionPM <pierre.mourret.cav@gmail.com>
2021-12-21 15:49:39 +0100

    Update main.php

--- main.php
+++ main.php
@@ -2,27 +2,27 @@
     include_once 'functions.php';
 
     $mbox = connectBox();
-    $mbox_sent = connectBoxSent();
+    $mboxSent = connectBoxSent();
 
-    if (!$mbox && !$mbox_sent){
+    if (!$mbox && !$mboxSent){
         die("Connexion impossible, vérifier vos identifiants");
     }
     else {
         echo "Connexion réussie";
 
         $checkBox = checkBox($mbox);
-        $checkBoxSent = checkBoxSend($mbox_sent);
+        $checkBoxSent = checkBoxSend($mboxSent);
 
         $mboxOverview = overviewBox($mbox,$checkBox);
-        $mboxOverviewSent = overviewBox($mbox_sent,$checkBoxSent);
+        $mboxOverviewSent = overviewBox($mboxSent,$checkBoxSent);
 
         $sortReceived = sortReceived($mbox,$mboxOverview);
-        $sortSent = sortSent($mbox_sent,$mboxOverviewSent,$sortReceived);
+        $sortSent = sortSent($mboxSent,$mboxOverviewSent,$sortReceived);
         echo "<pre>";
             print_r($sortReceived);
             print_r($sortSent);
         echo "</pre>";
     }
     
-    closeMbox($mbox,$mbox_sent);
+    closeMbox($mbox,$mboxSent);
 ?>
